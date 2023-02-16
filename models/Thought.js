e3ae2b17d6f51db71f00e5463207fbf19fdf552e
Anton Krasnikov <krasnikovanton84@gmail.com>
2023-02-16 00:47:29 -0800

    Adds reactionsSchema inside Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,17 +1,46 @@
 const { Schema, model } = require('mongoose');
 const { DateTime } = require("luxon");
-const reactionSchema = require('./Reaction');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+      },
+    reactionBody: {type: String, required: true, maxLength: 280},
+    username: {type: String, required: true}, 
+    createdAt: {
+        type: Date, 
+        default: DateTime.now(),
+        get: formatedDate,
+      }
+  },
+  {
+    toJSON: {
+      getters: true
+    },
+  }
+);
+
+function formatedDate(createdAt) {
+  return createdAt.toLocaleString(DateTime.DATE_MED);
+}
+
 //crearing Thought schema
 const thoughtSchema = new Schema(
   {
     thoughtText: {type: String, required: true, minLength: 1, maxLength: 280},
-    createdAt: {type: Date, default: DateTime.now().toLocaleString(DateTime.DATE_MED)},
+    createdAt: {type: Date, 
+      default: DateTime.now(),
+      get: formatedDate,
+    },
     username: {type: String, required: true},
-    reactions: [reactionSchema.schema],
+    reactions: [reactionSchema],
   },
   {
     toJSON: {
       virtuals: true,
+      getters: true
     },
     id: false,
   }
@@ -20,7 +49,7 @@ const thoughtSchema = new Schema(
 thoughtSchema
   .virtual('reactionCount')
   .get(function () {
-    return this.reactions.length;
+    return (this.reactions!==undefined)?this.reactions.length:0;
   });
 
 const Thought = model('thought', thoughtSchema);
